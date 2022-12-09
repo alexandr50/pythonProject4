84762efbe2c2282c27138bbfd9cba75f368727c1
alexandr50 <dmitrymaslov2016@mail.ru>
2022-12-09 15:32:34 +0300

    commit

--- framework/templates.py
+++ framework/templates.py
@@ -2,12 +2,7 @@ from jinja2 import Template
 import os
 
 def render_(template_name, folder='templates', **kwargs):
-    """
-    :param template_name: имя шаблона
-    :param folder: папка в которой ищем шаблон
-    :param kwargs: параметры
-    :return:
-    """
+
     file_path = os.path.join(folder, template_name)
     # Открываем шаблон по имени
     with open(file_path, encoding='utf-8') as f:
